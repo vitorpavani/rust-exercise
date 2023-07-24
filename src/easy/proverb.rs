3b5cc4fdde75e4c9d8f44ab502d95934bf1ee585
Vitor Pavan <vitor.pavani@poswise.de>
2023-07-24 15:07:43 +0200

    proverb and cargo-watch

---
+++ src/easy/proverb.rs
@@ -0,0 +1,16 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    let mut proverb = String::new();
+    if list.len() == 0 {
+        return proverb;
+    }
+
+    for i in 0..list.len() - 1 {
+        proverb.push_str(&format!(
+            "For want of a {} the {} was lost.\n",
+            list[i],
+            list[i + 1]
+        ));
+    }
+    proverb.push_str(&format!("And all for the want of a {}.", list[0]));
+    return proverb;
+}
